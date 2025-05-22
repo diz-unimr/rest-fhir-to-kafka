2f01db666752c25f34627bc506323f3d2e4f0a90
Jakub Lidke <jakub.lidke@uni-marburg.de>
2025-05-22 16:09:51 +0200

    test: fix unit test ToTopicProducer.receiveFhirData

--- src/test/java/de/unimarburg/diz/rest_fhir_to_kafka/input/RestFhirControllerTest.java
+++ src/test/java/de/unimarburg/diz/rest_fhir_to_kafka/input/RestFhirControllerTest.java
@@ -1,17 +1,21 @@
 /* GNU AFFERO GENERAL PUBLIC LICENSE  Version 3 (C)2025 */
 package de.unimarburg.diz.rest_fhir_to_kafka.input;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import ca.uhn.fhir.context.FhirContext;
+import de.unimarburg.diz.rest_fhir_to_kafka.output.ToTopicProducer;
 import org.hl7.fhir.r4.model.HumanName;
 import org.hl7.fhir.r4.model.Patient;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -21,6 +25,9 @@ class RestFhirControllerTest {
   @Autowired MockMvc mockMvc;
 
   @Autowired FhirContext fhirContext;
+
+  @MockitoBean(enforceOverride = true)
+  private ToTopicProducer toTopicProducer;
 
   @Test
   void receiveFhirData() throws Exception {
@@ -32,6 +39,8 @@ class RestFhirControllerTest {
             new Patient()
                 .setActive(true)
                 .addName(new HumanName().addGiven("Lana").setFamily("Musterfrau")));
+
+    when(toTopicProducer.sendTopic(any())).thenReturn(true);
 
     mockMvc
         .perform(post("/fhir").contentType(APPLICATION_XML_VALUE).content(xmlResource))
